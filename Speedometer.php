062ed6d60204d7ef96e7a37fc457f359971f1741
Naïm Jhuboo <30967014+Fredersen@users.noreply.github.com>
2021-11-22 12:55:55 +0100

    Update Speedometer.php

--- Speedometer.php
+++ Speedometer.php
@@ -4,12 +4,20 @@ class Speedometer
 {
 
     public const KMTOMILES_CONVERSION = 0.621;
+    public const MILESTOKM_CONVERSION = 1.609;
 
     public static function convertKmToMiles(int $km): float
     {   
         return $km * self::KMTOMILES_CONVERSION;
     }
 
+    public static function convertMilesToKm(int $miles): float
+    {   
+        return $miles * self::MILESTOKM_CONVERSION;
+    }
+
+
 }
 
 echo Speedometer::convertKmToMiles(10);
+echo Speedometer::convertMilesToKm(10);
